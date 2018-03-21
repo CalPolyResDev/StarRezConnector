a705bea779151ca91e0eb8b990f614ae51340439
fedorareis <fedorareis@gmail.com>
2018-03-21 16:25:04 -0700

    There is no good way of defining a dependancy from another repo.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open(os.path.join('starrezconnector', '__init__.py')) as open_file:
 readme = open('README.rst').read()
 changes = open('CHANGES.rst').read().replace('.. :changelog:', '')
 
-INSTALL_REQUIRES = [starrez_client]
+INSTALL_REQUIRES = []
 
 setup(
     name=finder.title,
